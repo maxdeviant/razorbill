5122035113c8d7811ab1cc09c9ead3b68234e67f
Marshall Bowers <elliott.codes@gmail.com>
2024-07-27 17:20:47 -0400

    Add tests for `FileInfo`

--- crates/razorbill/src/content/file_info.rs
+++ crates/razorbill/src/content/file_info.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct FileInfo {
     pub path: PathBuf,
     pub parent: PathBuf,
@@ -27,5 +27,45 @@ impl FileInfo {
             .components()
             .map(|component| component.as_os_str().to_string_lossy().to_string())
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_file_info() {
+        let file = FileInfo::new("content", "content/_index.md");
+        assert_eq!(
+            file,
+            FileInfo {
+                path: PathBuf::from("content/_index.md"),
+                parent: PathBuf::from("content"),
+                components: vec![]
+            }
+        );
+
+        let file = FileInfo::new("content", "content/a/b/c/d/_index.md");
+        assert_eq!(
+            file,
+            FileInfo {
+                path: PathBuf::from("content/a/b/c/d/_index.md"),
+                parent: PathBuf::from("content/a/b/c/d"),
+                components: vec!["a".into(), "b".into(), "c".into(), "d".into()]
+            }
+        );
+
+        let file = FileInfo::new("some/other/path", "some/other/path/blog/hello-world.md");
+        assert_eq!(
+            file,
+            FileInfo {
+                path: PathBuf::from("some/other/path/blog/hello-world.md"),
+                parent: PathBuf::from("some/other/path/blog"),
+                components: vec!["blog".into()]
+            }
+        );
     }
 }
